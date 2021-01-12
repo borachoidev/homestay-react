17ee7f78d7f12a427ae776474f4419ad048fdf53
september21st <september21st@users.noreply.github.com>
2021-01-12 10:03:46 +0900

    init commit

---
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/KoreaTourApplication.java
@@ -0,0 +1,13 @@
+package com.bitcamp.korea_tour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoreaTourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoreaTourApplication.class, args);
+	}
+
+}
