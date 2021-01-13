518bfa6cfde827b4c598364eb41eccdf922d1c92
september21st <september21st@users.noreply.github.com>
2021-01-13 15:15:44 +0900

    front thyleaf

--- korea_tour/src/main/java/boot/tour/maincontroller/MainController.java
+++ korea_tour/src/main/java/boot/tour/maincontroller/MainController.java
@@ -3,6 +3,7 @@ package boot.tour.maincontroller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import boot.tour.data.MysqlTourMapper;
 
@@ -12,9 +13,11 @@ public class MainController {
 	@Autowired
 	MysqlTourMapper mapper;
 	
-	@GetMapping("/main")
-	public String getMain() {
-		
-		return "main";
+	@GetMapping("/")
+	public ModelAndView getMain() {
+		ModelAndView model=new ModelAndView();
+		model.setViewName("home/home");
+		model.addObject("name", "bitcamp");
+		return model;
 	}
 }
