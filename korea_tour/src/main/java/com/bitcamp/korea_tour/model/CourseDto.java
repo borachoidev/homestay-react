bf052edcb2f514de1dcfbff8c8b763c40ef5aa1c
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-01-14 23:03:20 +0900

    placeAnswer query문 작성

--- korea_tour/src/main/java/com/bitcamp/korea_tour/model/CourseDto.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/model/CourseDto.java
@@ -6,11 +6,12 @@ import lombok.Data;
 public class CourseDto {
 	private String courseNum;
 	private String name;
-	private String customTag;
-	private String selectTag;
 	private String content;
 	private int viewOrNot;
 	private String contentIds;
 	private int timesOfShare;
 	private String userNum;
+	private String who;
+	private String during;
+	private String how;
 }
