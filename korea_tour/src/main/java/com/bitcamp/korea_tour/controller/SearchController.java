5ab73273ea291c932bacd43f5972c2292966d1f8
KIM-KYOUNG-OH <66231761+KIM-KYOUNG-OH@users.noreply.github.com>
2021-01-15 14:09:55 +0900

    no message

---
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/SearchController.java
@@ -0,0 +1,38 @@
+package com.bitcamp.korea_tour.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class SearchController {
+
+	// 현재 month
+	SimpleDateFormat format = new SimpleDateFormat("MM");
+	Calendar time = Calendar.getInstance();
+	int currentMonth = Integer.parseInt(format.format(time));
+	
+	@GetMapping("/search")
+	public ModelAndView getCourseMain(@RequestParam String keyword) { // 검색어를 파라미터로 받는다
+		if(currentMonth>=3 && currentMonth<6) {
+			keyword="봄";
+		}else if(currentMonth>=6 && currentMonth<9) {
+			keyword="여름";
+		}else if(currentMonth>=9 && currentMonth<12) {
+			keyword="가을";
+		}else {
+			keyword="겨울";
+		}
+		
+		ModelAndView mview = new ModelAndView();
+		mview.addObject("keyword", keyword);
+		mview.setViewName("search/main");
+		return mview;
+	}
+	
+	
+}
