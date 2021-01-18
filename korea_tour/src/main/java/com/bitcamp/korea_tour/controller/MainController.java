bcdcb9a1fc3ef3ca559972a210e8f2e5296701a6
Gayeong Do <dodoga096@gmail.com>
2021-01-19 03:03:19 +0900

    수정안된 부분 재수정(최종 세팅)

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/MainController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/MainController.java
@@ -2,24 +2,14 @@ package com.bitcamp.korea_tour.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-<<<<<<< HEAD
-=======
-import com.bitcamp.korea_tour.model.mapper.MysqlTourMapper;
->>>>>>> 1a877bca3817169b1b92de358faec21e416ecfad
 
 @Controller
 public class MainController {
 	
-<<<<<<< HEAD
-	@GetMapping("/")
-	public String getMain() {
-		return "home";
-=======
 	@GetMapping({"/index","/"})
-	public String getMain() {
+	public String getMain() { 
 		
 		return "home/home";
->>>>>>> 1a877bca3817169b1b92de358faec21e416ecfad
 	}
 	
 }
