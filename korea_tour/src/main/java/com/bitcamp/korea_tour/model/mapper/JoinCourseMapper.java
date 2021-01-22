e0ec3832cabab42c42aaf6e0e96c0306ca5c08ab
Gayeong Do <dodoga096@gmail.com>
2021-01-22 17:34:26 +0900

    코스 쿼리 수정, 매퍼 수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/model/mapper/JoinCourseMapper.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/model/mapper/JoinCourseMapper.java
@@ -17,6 +17,10 @@ public interface JoinCourseMapper {
 	모든코스 모아보기(인기순) 
 	Nav로 태그 검색한 코스보기(최신순)
 	Nav로 태그 검색한 코스보기(인기순) 
+	통합검색코스(검색어에 해당하는 관광지가 들어있는 코스까지) 검색쿼리 최신순
+	통합겸색코스(검색어에 해당하는 관광지가 들어있는 코스까지) 검색쿼리 인기순
+	맞춤코스 검색(최신순)
+	맞춤코스 검색(인기순)
 	*/
 	
 	List<JoinCourseDto> getMyCourseList(int loginNum);  //세션 로그인num 받아오기
@@ -26,4 +30,8 @@ public interface JoinCourseMapper {
 	List<JoinCourseDto> getAllCourseByLike();
 	List<JoinCourseDto> getTagCourseByTime(Map<String, String> tag);
 	List<JoinCourseDto> getTagCourseByLike(Map<String, String> tag);
+	List<JoinCourseDto> getSearchCourseByTime(String keyword);
+	List<JoinCourseDto> getSearchCourseByLike(String keyword);
+	List<JoinCourseDto> getCustomCourseByTime(Map<String, String> tag);
+	List<JoinCourseDto> getCustomCourseByLike(Map<String, String> tag);
 }
