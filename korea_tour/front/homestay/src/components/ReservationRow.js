fe947a3373f66d035b857d59744a96a3f9091727
DESKTOP-SLBSUFE\user <tkznzl12@naver.com>
2021-01-27 11:55:20 +0900

    tourplace&homestay review 관련 component

---
+++ korea_tour/front/homestay/src/components/ReservationRow.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ReservationRow extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default ReservationRow;
---
+++ korea_tour/front/homestay/src/components/StarRating.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Box from '@material-ui/core/Box';
+
+import AllStarRating from './AllStarRating';
+
+const labels = {
+  0.5: '0.5',
+  1: '1.0',
+  1.5: '1.5',
+  2: '2.0',
+  2.5: '2.5',
+  3: '3.0',
+  3.5: '3.5',
+  4: '4.0',
+  4.5: '4.5',
+  5: '5.0',
+};
+
+const useStyles = makeStyles({
+  root: {
+    width: 200,
+    display: 'flex',
+    alignItems: 'center',
+  },
+});
+
+export default function HoverRating(props) {
+  const [value, setValue] = React.useState(2);
+  const [hover, setHover] = React.useState(-1);
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Rating
+        name={props.name}
+        value={value}
+        precision={0.5}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        onChangeActive={(event, newHover) => {
+          setHover(newHover);
+        }}
+      />
+      {value !== null && <Box ml={2}>{labels[hover !== -1 ? hover : value]}</Box>}
+    </div>
+  );
+}
