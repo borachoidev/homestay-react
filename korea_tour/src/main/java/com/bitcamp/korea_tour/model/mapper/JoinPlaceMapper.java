6d728779188c7d2d221ca22ff00613c3eef466a4
DESKTOP-GS2EKTP\user <f>
2021-01-27 11:31:38 +0900

    마이페이지 컨트롤러 댓글,관광지 노티스컨트롤러 수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/model/mapper/JoinPlaceMapper.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/model/mapper/JoinPlaceMapper.java
@@ -13,25 +13,27 @@ import com.bitcamp.korea_tour.model.PlaceApiPhotoDto;
 import com.bitcamp.korea_tour.model.PlaceDto;
 import com.bitcamp.korea_tour.model.PlacePhotoDto;
 import com.bitcamp.korea_tour.model.TourAnswerDto;
+import com.bitcamp.korea_tour.model.joinPlaceDto;
 
 @Mapper
 public interface JoinPlaceMapper {
-	int getTotalPlaceMark(int userNum);
-	List<PlaceDto> getFourDatasByRandom();
-	int getTotalCount();
-	int getTotalCountInArea(int areaCode);
-	List<JoinPlaceListDto> searchPlaceByTitleInArea(HashMap<String, Object> map);
-	List<JoinPlaceListDto> searchPlaceByTitleInNation(HashMap<String, Object> map);
-	List<JoinPlaceListDto> searchPlaceByLikeInArea(HashMap<String, Object> map);
-	List<JoinPlaceListDto> searchPlaceByLikeInNation(HashMap<String, Object> map);
-	PlaceDto getPlaceDetail(int contentId);
-	List<PlaceApiPhotoDto> getPlaceDetailApiPhotos(int contentId);
-	List<PlacePhotoDto> getPlaceDetailPhotos(int contentId);
-	List<TourAnswerDto> getAnswerOfPlace(int contentId);
-	List<CourseDto> getCourseByUser(int userNum);
-	void insertPlaceIntoCourse(CoursePlaceDto dto);
-	int getMaxOrderNum(int coursePlaceNum);
-	int getTotalCountByKeywordSearch(String keyword);
-	List<JoinPlaceListDto> keywordSearchPlaceByTitle(HashMap<String, Object> map);
-	List<JoinPlaceListDto> keywordSearchPlaceBylike(HashMap<String, Object> map);
+   List<joinPlaceDto> getTotalPlaceMark(int loginNum, HashMap<String, Object> map);
+   int getTotalCountMyPlaceMark(int userNum);
+   List<PlaceDto> getFourDatasByRandom();
+   int getTotalCount();
+   int getTotalCountInArea(int areaCode);
+   List<JoinPlaceListDto> searchPlaceByTitleInArea(HashMap<String, Object> map);
+   List<JoinPlaceListDto> searchPlaceByTitleInNation(HashMap<String, Object> map);
+   List<JoinPlaceListDto> searchPlaceByLikeInArea(HashMap<String, Object> map);
+   List<JoinPlaceListDto> searchPlaceByLikeInNation(HashMap<String, Object> map);
+   PlaceDto getPlaceDetail(int contentId);
+   List<PlaceApiPhotoDto> getPlaceDetailApiPhotos(int contentId);
+   List<PlacePhotoDto> getPlaceDetailPhotos(int contentId);
+   List<TourAnswerDto> getAnswerOfPlace(int contentId);
+   List<CourseDto> getCourseByUser(int userNum);
+   void insertPlaceIntoCourse(CoursePlaceDto dto);
+   int getMaxOrderNum(int coursePlaceNum);
+   int getTotalCountByKeywordSearch(String keyword);
+   List<JoinPlaceListDto> keywordSearchPlaceByTitle(HashMap<String, Object> map);
+   List<JoinPlaceListDto> keywordSearchPlaceBylike(HashMap<String, Object> map);
 }
--- korea_tour/src/main/java/com/bitcamp/korea_tour/model/service/TourAnswerServiceImpl.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/model/service/TourAnswerServiceImpl.java
@@ -1,5 +1,6 @@
 package com.bitcamp.korea_tour.model.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -12,114 +13,144 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class TourAnswerServiceImpl implements TourAnswerService{
-	
-	private final TourAnswerMapper mapper;
-	@Override
-	public void insertPlaceAnswer(TourAnswerDto dto) {
-		// TODO Auto-generated method stub
-		mapper.insertPlaceAnswer(dto);
-	}
+   
+   private final TourAnswerMapper mapper;
+   @Override
+   public void insertPlaceAnswer(TourAnswerDto dto) {
+      // TODO Auto-generated method stub
+      mapper.insertPlaceAnswer(dto);
+   }
 
-	@Override
-	public void deletePlaceAnswerByUser(int tourAnswerNum) {
-		// TODO Auto-generated method stub
-		mapper.deletePlaceAnswerByUser(tourAnswerNum);
-	}
+   @Override
+   public void deletePlaceAnswerByUser(int tourAnswerNum) {
+      // TODO Auto-generated method stub
+      mapper.deletePlaceAnswerByUser(tourAnswerNum);
+   }
 
-	@Override
-	public void deletePlaceAnswerByAdmin(int tourAnswerNum) {
-		// TODO Auto-generated method stub
-		mapper.deletePlaceAnswerByAdmin(tourAnswerNum);
-	}
+   @Override
+   public void deletePlaceAnswerByAdmin(int tourAnswerNum) {
+      // TODO Auto-generated method stub
+      mapper.deletePlaceAnswerByAdmin(tourAnswerNum);
+   }
 
-	@Override
-	public List<TourAnswerDto> getAnswerOfPlace(int contentId) {
-		// TODO Auto-generated method stub
-		return mapper.getAnswerOfPlace(contentId);
-	}
+   @Override
+   public List<TourAnswerDto> getAnswerOfPlace(int contentId) {
+      // TODO Auto-generated method stub
+      return mapper.getAnswerOfPlace(contentId);
+   }
 
-	@Override
-	public List<TourAnswerDto> getUserAnswerPlace(int loginNum, int start, int perPage) {
-		// TODO Auto-generated method stub
-		return mapper.getUserAnswerPlace(loginNum);
-	}
 
-	@Override
-	public List<TourAnswerDto> getUserReAnswerPlace(int loginNum, int start, int perPage) {
-		// TODO Auto-generated method stub
-		return mapper.getUserReAnswerPlace(loginNum);
-	}
+   @Override
+   public void insertCourseAnswer(TourAnswerDto dto) {
+      // TODO Auto-generated method stub
+      mapper.insertCourseAnswer(dto);
+   }
 
-	@Override
-	public void insertCourseAnswer(TourAnswerDto dto) {
-		// TODO Auto-generated method stub
-		mapper.insertCourseAnswer(dto);
-	}
+   @Override
+   public void deleteCourseAnswerByUser(int tourAnswerNum) {
+      // TODO Auto-generated method stub
+      mapper.deleteCourseAnswerByUser(tourAnswerNum);
+   }
 
-	@Override
-	public void deleteCourseAnswerByUser(int tourAnswerNum) {
-		// TODO Auto-generated method stub
-		mapper.deleteCourseAnswerByUser(tourAnswerNum);
-	}
+   @Override
+   public void deleteCourseAnswerByAdmin(int tourAnswerNum) {
+      // TODO Auto-generated method stub
+      mapper.deleteCourseAnswerByAdmin(tourAnswerNum);
+   }
 
-	@Override
-	public void deleteCourseAnswerByAdmin(int tourAnswerNum) {
-		// TODO Auto-generated method stub
-		mapper.deleteCourseAnswerByAdmin(tourAnswerNum);
-	}
+   @Override
+   public List<TourAnswerDto> getAnswerOfCourse(int courseNum) {
+      // TODO Auto-generated method stub
+      return mapper.getAnswerOfCourse(courseNum);
+   }
 
-	@Override
-	public List<TourAnswerDto> getAnswerOfCourse(int courseNum) {
-		// TODO Auto-generated method stub
-		return mapper.getAnswerOfCourse(courseNum);
-	}
 
-	@Override
-	public List<TourAnswerDto> getUserAnswerCourse(int loginNum, int start, int perPage) {
-		// TODO Auto-generated method stub
-		return mapper.getUserAnswerCourse(loginNum);
-	}
+   @Override
+   public List<TourAnswerDto> getAdminAnswer() {
+      // TODO Auto-generated method stub
+      return mapper.getAdminAnswer();
+   }
 
-	@Override
-	public List<TourAnswerDto> getUserReAnswerCourse(int loginNum, int start, int perPage) {
-		// TODO Auto-generated method stub
-		return mapper.getUserAnswerCourse(loginNum);
-	}
+   @Override
+   public List<TourAnswerDto> getAdminReAnswer() {
+      // TODO Auto-generated method stub
+      return mapper.getAdminReAnswer();
+   }
 
-	@Override
-	public List<TourAnswerDto> getAdminAnswer() {
-		// TODO Auto-generated method stub
-		return mapper.getAdminAnswer();
-	}
+   @Override
+   public int getTotalCountPlaceAnswer(int loginNum) {
+      // TODO Auto-generated method stub
+      return mapper.getTotalCountPlaceAnswer(loginNum);
+   }
 
-	@Override
-	public List<TourAnswerDto> getAdminReAnswer() {
-		// TODO Auto-generated method stub
-		return mapper.getAdminReAnswer();
-	}
+   @Override
+   public int getTotalCountPlaceReAnswer(int loginNum) {
+      // TODO Auto-generated method stub
+      return mapper.getTotalCountPlaceReAnswer(loginNum);
+   }
 
-	@Override
-	public int getTotalCountPlaceAnswer(int loginNum) {
-		// TODO Auto-generated method stub
-		return mapper.getTotalCountPlaceAnswer(loginNum);
-	}
+   @Override
+   public int getTotalCountCourseAnswer(int loginNum) {
+      // TODO Auto-generated method stub
+      return mapper.getTotalCountCourseAnswer(loginNum);
+   }
 
-	@Override
-	public int getTotalCountPlaceReAnswer(int loginNum) {
-		// TODO Auto-generated method stub
-		return mapper.getTotalCountPlaceReAnswer(loginNum);
-	}
+   @Override
+   public int getTotalCountCourseReAnswer(int loginNum) {
+      // TODO Auto-generated method stub
+      return mapper.getTotalCountCourseReAnswer(loginNum);
+   }
 
-	@Override
-	public int getTotalCountCourseAnswer(int loginNum) {
-		// TODO Auto-generated method stub
-		return mapper.getTotalCountCourseAnswer(loginNum);
-	}
 
-	@Override
-	public int getTotalCountCourseReAnswer(int loginNum) {
-		// TODO Auto-generated method stub
-		return mapper.getTotalCountCourseReAnswer(loginNum);
-	}
+@Override
+public List<TourAnswerDto> getUserAnswer(int loginNum, HashMap<String, Object> map) {
+   // TODO Auto-generated method stub
+   return mapper.getUserAnswer(loginNum, map);
+}
+
+@Override
+public List<TourAnswerDto> getUserReAnswer(int loginNum, HashMap<String, Object> map) {
+   // TODO Auto-generated method stub
+   return mapper.getUserReAnswer(loginNum, map);
+}
+
+@Override
+public int getTotalCountAnswer(int loginNum) {
+   // TODO Auto-generated method stub
+   return mapper.getTotalCountAnswer(loginNum);
+}
+
+@Override
+public int getTotalCountReAnswer(int loginNum) {
+   // TODO Auto-generated method stub
+   return mapper.getTotalCountReAnswer(loginNum);
+}
+
+@Override
+public void insertPlaceReAnswer(TourAnswerDto dto) {
+   /*
+    * int num=dto.getTourAnswerNum(); int regroup=dto.getRegroup(); int
+    * relevel=dto.getRelevel();
+    * 
+    * if(num==null) { regroup=this.getMaxNum()+1; relevel=0;
+    * 
+    * 
+    * }else { this.updateRelevelOfAnswer(regroup, relevel); relevel+=1; regroup+=1;
+    * }
+    */
+   
+   // TODO Auto-generated method stub
+    mapper.insertPlaceReAnswer(dto);
+   
+}
+
+@Override
+public void insertCourseReAnswer(TourAnswerDto dto) {
+   // TODO Auto-generated method stub
+   mapper.insertCourseReAnswer(dto);
+   
+}
+
+
 
 }
