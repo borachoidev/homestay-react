dc68ec651f9bc85d6c3a5cea9acb4a61b2f644b4
dami7177 <57148660+dami7177@users.noreply.github.com>
2021-01-27 20:30:54 +0900

    Revert "컨트롤러들 수정"
    This reverts commit 066f9e0b053ef88d8ef87fd88e899ccb80b86d8a.

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/MainSearchController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/MainSearchController.java
@@ -1,8 +1,11 @@
 package com.bitcamp.korea_tour.controller;
 
+<<<<<<< HEAD
+=======
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+>>>>>>> ff8b42ca0c08c761da49a49961d00bc36b44c786
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,47 +13,57 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainSearchController {
-   SimpleDateFormat sdf = new SimpleDateFormat("MM");
-   Date currDate = new Date();
-   int currentMonth = Integer.parseInt(sdf.format(currDate));
-   
-   @GetMapping("/search")
-   public String goSearch(
-         @RequestParam(defaultValue="1") int currentPage,
-         @RequestParam(required=false)String keyword,Model model
-         ) {
-      if(keyword==null) {
-         System.out.println("키워드가 널!!");
-         if(currentMonth>=3 && currentMonth<6) {
-            keyword="봄";
-         }else if(currentMonth>=6 && currentMonth<9) {
-            keyword="여름";
-         }else if(currentMonth>=9 && currentMonth<12) {
-            System.out.println(currentMonth);
-            keyword="가을";
-         }else {
-            keyword="겨울";
-         }
-      }
-      model.addAttribute("currentPage", currentPage);
-      model.addAttribute("keyword", keyword);   
-      return "search/result";
-   }
-   
-   @GetMapping("/course.search")
-   public String goCourseSearch(
-         @RequestParam(defaultValue="1") int currentPage,
-         @RequestParam String who,
-         @RequestParam String during,
-         @RequestParam String how,Model model
-         ) {
-      
-      model.addAttribute("currentPage", currentPage);
-      model.addAttribute("who", who);
-      model.addAttribute("during", during);
-      model.addAttribute("how", how);
-      return "course/search";
-      
-      
-   }
+<<<<<<< HEAD
+	
+	@GetMapping("/search")
+	public String goSearch(@RequestParam(defaultValue="1") int currentPage,@RequestParam String keyword,Model model ) {
+		
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("keyword", keyword);	
+		return "/search/allsearch";
+=======
+	SimpleDateFormat sdf = new SimpleDateFormat("MM");
+	Date currDate = new Date();
+	int currentMonth = Integer.parseInt(sdf.format(currDate));
+	
+	@GetMapping("/search")
+	public String goSearch(
+			@RequestParam(defaultValue="1") int currentPage,
+			@RequestParam(required=false)String keyword,Model model
+			) {
+		if(keyword==null) {
+			System.out.println("키워드가 널!!");
+			if(currentMonth>=3 && currentMonth<6) {
+				keyword="봄";
+			}else if(currentMonth>=6 && currentMonth<9) {
+				keyword="여름";
+			}else if(currentMonth>=9 && currentMonth<12) {
+				System.out.println(currentMonth);
+				keyword="가을";
+			}else {
+				keyword="겨울";
+			}
+		}
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("keyword", keyword);	
+		return "search/result";
+	}
+	
+	@GetMapping("/course.search")
+	public String goCourseSearch(
+			@RequestParam(defaultValue="1") int currentPage,
+			@RequestParam String who,
+			@RequestParam String during,
+			@RequestParam String how,Model model
+			) {
+		
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("who", who);
+		model.addAttribute("during", during);
+		model.addAttribute("how", how);
+		return "course/search";
+		
+		
+>>>>>>> ff8b42ca0c08c761da49a49961d00bc36b44c786
+	}
 }
