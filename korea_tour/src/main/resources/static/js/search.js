eb709a9e8db2133044db6d44592bc52f7f98b21a
september21st <september21st@users.noreply.github.com>
2021-01-28 13:14:31 +0900

    검색 페이지 페이징처리

--- korea_tour/src/main/resources/static/js/search.js
+++ korea_tour/src/main/resources/static/js/search.js
@@ -1,151 +1,103 @@
 'use strict';
 
 let keyword = document.querySelector('.keyword-title').getAttribute('keyword');
-
-parseAreaBased(1, keyword);
-// getAreaName(areaCode);
-
-/* ----- functions ---- */
-
-//eventEndDate 구하기
-
+let sort = 'title';
+let perBlock = 5;
+let currentPage = document
+  .querySelector('.pagination')
+  .getAttribute('currentPage');
+let startPage = Math.floor((currentPage - 1) / perBlock) * perBlock + 1;
+let endPage = startPage + perBlock - 1;
+searchAllbyTitle(sort, keyword, currentPage);
+areaOnclick();
 function areaOnclick() {
-  let areaList = document.querySelectorAll('.area-list');
-  for (const areaBtn of areaList) {
-    areaBtn.addEventListener('click', function (e) {
-      areaCode = e.target.getAttribute('area');
-      if (areaCode == 'all') {
-        areaCode = '';
-      }
-      const children = areaBtn.parentElement.children;
+  let sorts = document.querySelectorAll('.sort');
+  for (const sort of sorts) {
+    sort.addEventListener('click', function (e) {
+      let sortCode = e.target.getAttribute('value');
+
+      const children = sort.parentElement.children;
       for (const child of children) {
         child.classList.remove('active');
       }
+      console.log(sort);
+      sort.classList.add('active');
+      // if (sortCode == 'title') {
 
-      areaBtn.classList.add('active');
-      parseAreaBased(pageNum, keyword);
-      getAreaName(areaCode);
+      // } else {
+      // }
+      searchAllbyTitle(sortCode, keyword, currentPage);
     });
   }
 }
-// // parameter value 읽기
-// function getParam(key) {
-//   let param;
-//   const queryString = window.location.search;
-//   const urlParams = new URLSearchParams(queryString);
-//   param = urlParams.get(key);
-//   return param;
-// }
-
-//areacode 변환
-// function getAreaName(areaCode) {
-//   let xmlStr;
-//   let xmlDoc;
-//   var xhr = new XMLHttpRequest();
-//   var url =
-//     'http://api.visitkorea.or.kr/openapi/service/rest/KorService/areaCode'; /*URL*/
-//   var queryParams =
-//     '?' +
-//     encodeURIComponent('serviceKey') +
-//     '=' +
-//     'CaXsuilSjIPz3L19P%2F6ufv6lKG6DwvhRg5x2lK5lzUTP66WyVxrNQcvBdb6CxuXHRNrbDXoscBHGwPy5aQd4sw%3D%3D'; /*Service Key*/
-//   queryParams +=
-//     '&' + encodeURIComponent('MobileOS') + '=' + encodeURIComponent('ETC'); /**/
-//   queryParams +=
-//     '&' +
-//     encodeURIComponent('MobileApp') +
-//     '=' +
-//     encodeURIComponent('test%20App'); /**/
-//   queryParams +=
-//     '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('20'); /**/
-//   queryParams +=
-//     '&' + encodeURIComponent('arrange') + '=' + encodeURIComponent('Y');
-//   queryParams +=
-//     '&' + encodeURIComponent('listYN') + '=' + encodeURIComponent('Y');
-
-//   console.log(url + queryParams);
-//   xhr.open('GET', url + queryParams);
-//   xhr.onreadystatechange = function () {
-//     if (this.readyState == 4) {
-//       xmlStr = this.responseText;
-//       var xmlParser, xmlDoc;
-//       xmlParser = new DOMParser();
-//       xmlDoc = xmlParser.parseFromString(xmlStr, 'text/xml');
-
-//       let list = xmlDoc.getElementsByTagName('item');
-//       let areaName;
-//       for (let i = 0; i < list.length; i++) {
-//         if (item[i].code === areaCode) areaName = item[i].name;
-//       }
-//       if (areaCode === '') {
-//         document.querySelector('.area-title').innerHTML = '전체보기';
-//       } else {
-//         document.querySelector('.area-title').innerHTML = areaName;
-//       }
-//     }
-//   };
-//   xhr.send(null);
-// }
 
 //api 데이터
-function parseAreaBased(pageNum, keyword) {
+function searchAllbyTitle(sort, keyword, pageNum) {
   if (pageNum == undefined) pageNum = 1;
-
+  let url;
   var xhr = new XMLHttpRequest();
   const keywordURL = decodeURIComponent(keyword);
-  var url = `search/title/${pageNum}/` + keywordURL;
-
+  if (sort == 'title') {
+    url = `search/title/${pageNum}/` + keywordURL;
+  } else {
+    url = `search/like/${pageNum}/` + keywordURL;
+  }
   /**/
-  console.log(url);
+  console.log(sort);
   xhr.open('GET', url);
   xhr.onreadystatechange = function () {
     if (this.readyState == 4) {
       let data = JSON.parse(this.responseText);
       let item = data.place;
-      console.log(item[0].areaCode);
       console.log(data);
 
-      // let totalCount = xmlDoc.getElementsByTagName('totalCount')[0]
-      //   .childNodes[0];
-      // totalPage = Math.ceil(parseInt(totalCount) / numOfRows);
-
-      // if (totalPage < pageNum) pageNum = totalPage;
-
       let n = '';
-      let contentId;
+
       for (let i = 0; i < item.length; i++) {
-        contentId = item[i].contentId;
+        const contentId = item[i].contentId;
         n += `<a href='/tourplace/detail?contentId=${contentId}'
           ><div class='card'>`;
         if (item[i].firstImage) {
-          n += `<img src='${item[i].firstImage}' class='thumbnail' contentid='${item[i].contentId}'/>`;
+          n += `<img src='${item[i].firstImage}' class='thumbnail' />`;
         } else {
           n += `<span class='thumbnail'>대표이미지 없음 🖼 </span>`;
         }
-        n += `<div class='info'><span class='title' contentid='${item[i].contentId}'>${item[i].title}</span>`;
+        n += `<div class='info'><span class='title' >${item[i].title}</span>`;
 
         n += `<span class='place'>${item[i].addr1}</span>`;
         n += `</div></div></a>`;
       }
       document.querySelector('.list').innerHTML = n;
-      //   if (totalCount == 0) {
-      //     document.querySelector('.list').innerHTML =
-      //       '<span class="alert-msg">해당하는 조건의 축제가 없습니다!😱</span>';
-      //   }
-      //페이징
-      //   let p = '';
-      //   for (let i = 0; i < totalPage; i++) {
-      //     p += `<li page='${i + 1}' class='page-list'>${i + 1}</li>`;
-      //   }
-      //document.querySelector('.pagination').innerHTML = p;
-      //   let pageList = document.querySelectorAll('.page-list');
-      //   for (const page of pageList) {
-      //     page.addEventListener('click', function (e) {
-      //       pageNum = e.target.getAttribute('page');
-      //       if (totalPage < pageNum) pageNum = totalPage;
-      //       parseAreaBased(areaCode, pageNum, numOfRows, month, currentYear);
-      //       getAreaName(areaCode);
-      //     });
+
+      //페이징 처리
+      const totalPage = data.totalPage; //
+
+      if (endPage > totalPage) {
+        endPage = totalPage;
+      }
+
+      if (item.length == 0) {
+        document.querySelector('.list').innerHTML =
+          '<span class="alert-msg">해당하는 검색 결과가 없습니다!😱</span>';
+      }
+
+      let p = '';
+      if (startPage > 1) {
+        p += `<li class='page-list'><a href='/search?keyword=${keyword}&currentPage=${
+          startPage - 1
+        }'><i class="fas fa-chevron-left"></i></li>`;
+      }
+      for (let i = startPage; i <= endPage; i++) {
+        p += `<li class='page-list'><a href='/search?keyword=${keyword}&currentPage=${i}'>${i}</a></li>`;
+      }
+      if (endPage < totalPage) {
+        p += `<li page='${
+          endPage + 1
+        }' class='page-list'><a href='/search?keyword=${keyword}&currentPage=${
+          endPage + 1
+        }'><i class="fas fa-chevron-right"></i></a></li>`;
+      }
+      document.querySelector('.pagination').innerHTML = p;
     }
   };
   xhr.send(null);
