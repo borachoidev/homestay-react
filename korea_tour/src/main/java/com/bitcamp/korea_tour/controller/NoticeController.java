b8ec7fcd2e2c3eafd53644373e53cbca8f8ab23f
DESKTOP-AVUBL5P\NC319 <olivia9842@gmail.com>
2021-01-29 15:02:25 +0900

    관리자 댓글출력 오류수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/NoticeController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/NoticeController.java
@@ -1,18 +1,11 @@
 package com.bitcamp.korea_tour.controller;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
