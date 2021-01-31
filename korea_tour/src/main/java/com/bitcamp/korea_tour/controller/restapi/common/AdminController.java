8b608c680e24f5d1cae67fa109314513193fd909
Gayeong Do <dodoga096@gmail.com>
2021-02-01 00:08:56 +0900

    관리자 컨트롤러 수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/common/AdminController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/common/AdminController.java
@@ -8,18 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bitcamp.korea_tour.controller.restapi.tour.PlaceController;
 import com.bitcamp.korea_tour.model.NoticeDto;
 import com.bitcamp.korea_tour.model.PlacePhotoDto;
 import com.bitcamp.korea_tour.model.TourAnswerDto;
-import com.bitcamp.korea_tour.model.service.NoticeService;
 import com.bitcamp.korea_tour.model.service.PlacePhotoService;
 import com.bitcamp.korea_tour.model.service.TourAnswerService;
 import com.bitcamp.korea_tour.model.service.paging.PagingService;
