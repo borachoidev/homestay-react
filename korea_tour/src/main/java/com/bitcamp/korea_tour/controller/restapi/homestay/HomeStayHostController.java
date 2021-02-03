49d6e12403f3c3d4a4ee4295df981e11f2366df4
DESKTOP-GS2EKTP\user <f>
2021-02-03 22:00:08 +0900

    호스트 집정보 수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/homestay/HomeStayHostController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/homestay/HomeStayHostController.java
@@ -2,8 +2,10 @@ package com.bitcamp.korea_tour.controller.restapi.homestay;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bitcamp.korea_tour.model.homestay.JoinHomeStayDetailDto;
 import com.bitcamp.korea_tour.model.service.homestay.HomeStayHostService;
 
 import lombok.RequiredArgsConstructor;
@@ -14,8 +16,10 @@ public class HomeStayHostController {
 	private final HomeStayHostService hsas;
 	
 	@PutMapping("/homestay/house/{homeStayNum}")
-	public void updateHouse(@PathVariable(value = "homeStayNum") int homeStayNum) {
-		hsas.updateHomeStay(homeStayNum);
-		hsas.updateHomeStayDetail(homeStayNum);
+	public void updateHouse(
+			@PathVariable(value = "homeStayNum") int homeStayNum,
+			@RequestBody JoinHomeStayDetailDto dto) {
+		hsas.updateHomeStay(dto, homeStayNum);
+		hsas.updateHomeStayDetail(dto, homeStayNum);
 	}
 }
