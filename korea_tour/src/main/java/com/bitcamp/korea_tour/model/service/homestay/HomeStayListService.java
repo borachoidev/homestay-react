aac5943ca108bfb1061c42e60fc759aa3a9e716c
Gayeong Do <dodoga096@gmail.com>
2021-02-03 22:47:28 +0900

    관리자 홈스테이 목록출력, 승인, 거절

--- korea_tour/src/main/java/com/bitcamp/korea_tour/model/service/homestay/HomeStayListService.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/model/service/homestay/HomeStayListService.java
@@ -2,6 +2,7 @@ package com.bitcamp.korea_tour.model.service.homestay;
 
 import java.util.List;
 
+import com.bitcamp.korea_tour.model.homestay.HomeStayDto;
 import com.bitcamp.korea_tour.model.homestay.HomeStayListDto;
 
 
@@ -11,4 +12,8 @@ public interface HomeStayListService {
 	String getHomeStayPhotoOfList(int homeStayNum);
 	int isMarked(int homeStayNum, int loginNum);
 	Double getAvgOfStar(int homeStayNum);
+	List<HomeStayDto> getAdminHomeStayList(int start, int perPage);
+	int getTotalAdminHomeStayList();
+	void approveHomeStay(int homeStayNum);
+	void denyHomeStay(int homeStayNum);
 }
