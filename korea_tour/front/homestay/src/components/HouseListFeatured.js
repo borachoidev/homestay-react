11124ad064cfbdeef5418c39c03aaace1fef2bab
dami7177 <57148660+dami7177@users.noreply.github.com>
2021-02-04 11:21:01 +0900

    관리자 - 승인대기 목록 출력 / 홈스테이 집목록카드리스트 출력

--- korea_tour/front/homestay/src/components/HouseListFeatured.js
+++ korea_tour/front/homestay/src/components/HouseListFeatured.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Grid from "@material-ui/core/Grid";
 import HouseListSort from "./HouseListSort";
 import HouseCard from "./HouseCard";
@@ -7,32 +8,64 @@ import Pagination from "./Pagination";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Container from "@material-ui/core/Container";
 
-export default function HouseListFeatured() {
-  return (
-    //여기에 for문 img src = url
+const HouseListFeatured = () =>{
+
+  const [contents,setContents] = useState(null);
+  const [loading,setLoading] = useState(false);
+
+
+useEffect(() =>{
+  // async를 사용하는 함수 따로 선언
+  const fatchData = async ()=> {
+      setLoading(true);
+      try {
+          const response = await axios.get(
+              'http://localhost:9003/homestays/price/1'
+          );
+          setContents(response.data.list);
+          console.log(response.data.list);
+          
+      } catch (e) {
+          console.log(e);
+      }
+      setLoading(false);
+  };
+  fatchData();
+}, []); 
+
+if (loading) {
+  return <p>대기중....</p>;
+}
+//articles 값이 설정되지 않았을때
+if (!contents) {
+return null;
+}
+// articles 값이 유효할때
+//<p>{contents[0].homeStayNum}</p>
+return (
+<div>
+   
+    {
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <HouseListSort />
       </Grid>
-      <Grid item xs={12} sm={6}>
-        <HouseCard url="https://source.unsplash.com/featured/?travel" />
-      </Grid>
-      <Grid item xs={12} sm={6}>
-        <HouseCard url="https://source.unsplash.com/featured/?nature" />
-      </Grid>
-      <Grid item xs={6} sm={4} md={3}>
-        <HouseCard url="https://source.unsplash.com/featured/?city" />
-      </Grid>
-      <Grid item xs={6} sm={4} md={3}>
-        <HouseCard url="https://source.unsplash.com/featured/?forest" />
-      </Grid>
-      <Grid item xs={6} sm={4} md={3}>
-        <HouseCard url="https://source.unsplash.com/featured/?mountains" />
-      </Grid>
-      <Grid item xs={6} sm={4} md={3}>
-        <HouseCard url="https://source.unsplash.com/featured/?island" />
-      </Grid>
+
+      {contents.map(content => (
+           <Grid item xs={6} sm={4} md={4} key={content}>
+             <HouseCard src={content.photoName} title={content.title}
+             addr1={content.addr1} price={content.price} countOfReview={content.countOfReview}
+             avgOfStar={content.avgOfStar} />
+             
+        </Grid>
+        
+        ))}
+        
+      
     </Grid>
-    
+    }
+     </div>
   );
 }
+
+export default HouseListFeatured;
