8b9c5676e35c688c349c8a2d9bead84e49eb13d6
dami7177 <57148660+dami7177@users.noreply.github.com>
2021-02-05 12:39:15 +0900

    다미 오류 지움

--- korea_tour/src/main/resources/static/js/hostlist.js
+++ korea_tour/src/main/resources/static/js/hostlist.js
@@ -38,7 +38,7 @@ function hostList(currentPage){
 					a+= `<td class="board_list_data">${item[i].homeStayNum}</td>`
 					a+=`<td class="board_list_data">${item[i].userName}</td>`
 					//포트가 달라서 연결이 안됨 추후 수정필요
-					a+=`<td class="board_list_data" num="${item[i].homeStayNum}"><a href="/homestay/adminhousedetail:${item[i].homeStayNum}">${item[i].title}</td>`
+					a+=`<td class="board_list_data" num="${item[i].homeStayNum}"><a href="/homestay/adminhousedetail/num=${item[i].homeStayNum}">${item[i].title}</td>`
 					a+=`<td class="board_list_data">${item[i].addr1}</td>`
 		}
 	document.querySelector(".list-low").innerHTML=a;
