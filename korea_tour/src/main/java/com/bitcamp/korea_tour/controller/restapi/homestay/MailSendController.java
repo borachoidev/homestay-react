17f4eaa8c483ea9aa77385a6673f0060a8dc915a
DESKTOP-GS2EKTP\user <f>
2021-02-05 18:10:59 +0900

    오류수정

--- korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/homestay/MailSendController.java
+++ korea_tour/src/main/java/com/bitcamp/korea_tour/controller/restapi/homestay/MailSendController.java
@@ -6,7 +6,6 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
