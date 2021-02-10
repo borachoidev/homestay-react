b1e5a137c1bc470d01d88e6c1f2a7c5601a20fc5
DESKTOP-SLBSUFE\user <tkznzl12@naver.com>
2021-02-10 16:38:00 +0900

    App.css 적용(기본 css)

--- korea_tour/front/homestay/src/App.js
+++ korea_tour/front/homestay/src/App.js
@@ -3,12 +3,17 @@ import { BrowserRouter } from 'react-router-dom';
 import RouteMain from './RouteMain';
 import NavBar from 'page/NavBar';
 
+import './App.css';
 function App() {
   return (
     <React.Fragment>
       <BrowserRouter basename="/homestay">
+        <div className="Nav">
         <NavBar />
+        </div>
+        <div className="Route">
         <RouteMain />
+        </div>
       </BrowserRouter>
     </React.Fragment>
   );
