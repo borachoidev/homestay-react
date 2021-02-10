239e7bd289628df4b2bf52ee6f73082381c1fced
september21st <september21st@users.noreply.github.com>
2021-02-10 13:58:54 +0900

    사진경로 추가

--- korea_tour/front/homestay/src/_utils/api.js
+++ korea_tour/front/homestay/src/_utils/api.js
@@ -1,1 +1,4 @@
-export const URL = 'http://localhost:9003/homestays';
+export const Base = 'http://localhost:9003';
+export const URL = `${Base}/homestays`;
+export const img = `${Base}/homeStayImg`;
+export const review = `${Base}/homeStayReviewImg`;
