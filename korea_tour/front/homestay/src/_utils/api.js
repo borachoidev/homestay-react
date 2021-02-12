b4f21a16f7e26ba09832e095cde08df6f5e856f2
september21st <september21st@users.noreply.github.com>
2021-02-13 01:26:10 +0900

    이미지경로 S2 수정

--- korea_tour/front/homestay/src/_utils/api.js
+++ korea_tour/front/homestay/src/_utils/api.js
@@ -1,4 +1,4 @@
-export const base = 'http://3.35.248.200';
+export const base = 'https://raonnadri.s3.ap-northeast-2.amazonaws.com';
 export const URL = `/homestays`;
 export const img = `${base}/homeStayImg`;
 export const review = `${base}/homeStayReviewImg`;
