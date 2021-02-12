bd17b9b2b3f4495a710717f4658102bb70dc4d22
Gayeong Do <dodoga096@gmail.com>
2021-02-12 16:22:05 +0900

    api 주소 변경 테스트

--- front/homestay/src/_utils/api.js
+++ front/homestay/src/_utils/api.js
@@ -1,4 +1,4 @@
-export const base = 'http://3.35.248.200:9003';
+export const base = 'http://3.35.248.200';
 export const URL = `/homestays`;
 export const img = `${base}/homeStayImg`;
 export const review = `${base}/homeStayReviewImg`;
